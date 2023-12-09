ae255937682359de62356339b79b0fd0816f4c41
NabinTako <nabintako999@gmail.com>
2023-12-09 12:33:16 +0545

    minor changes

--- CrackPassword.c
+++ CrackPassword.c
@@ -28,13 +28,9 @@ void crack(char *salt_and_encrypted)
   int x, y, z;    // Loop counters
   char salt[11];  // String used in hashing the password. Need space for \0 // incase you have modified the salt value, then should modifiy the number accordingly
   char plain[7]; // The combination of letters currently being checked // Please modifiy the number when you enlarge the encrypted password.
-  char passToDecrypt[98];
   char *enc;
   
-  strncpy(passToDecrypt,salt_and_encrypted,98);
-
   substr(salt, salt_and_encrypted, 0, 10);
-  salt[11]='\0';
 
 
 #pragma omp parallel for collapse(3) private(x, y, z, plain, enc) shared(count, passwordMatchFound)
@@ -58,13 +54,10 @@ void crack(char *salt_and_encrypted)
         
         count++;
         
-        // printf("#%-8d%s %s %s__%d\n", count, plain,salt, enc, omp_get_thread_num());
-
         if (strcmp(salt_and_encrypted, enc) == 0)
         {
           passwordMatchFound = 1;
           printf("#%-8d%s %s__%d\n", count, plain, enc, omp_get_thread_num());
-          // printf("\n\n\n\n\n\n\n");
           omp_unset_lock(&lock);
           #pragma omp cancel for
           continue;
@@ -80,7 +73,7 @@ void crack(char *salt_and_encrypted)
 int main()
 {
   int threadNnumber;
-  printf("Number of THreads to use: ");scanf("%d",&threadNnumber);
+  printf("Number of Threads to use: ");scanf("%d",&threadNnumber);
 
   struct timespec start, finish;
   long long int difference;
@@ -93,19 +86,16 @@ int main()
   // passing the encrypted password to the crack function to decode it
 
 
-  // crack("$6$WhyN0t$VdZOpOx2hPYncxxdpQ1d4F1pRJnQ0lsLLKts3oKIPxfD5CVEMDMq43CIUjiBCgOcHE1/HRoOIMeTiBDLYrhWt/"); // TN09
+  // crack("$6$WhyN0t$VdZOpOx2hPYncxxdpQ1d4F1pRJnQ0lsLLKts3oKIPxfD5CVEMDMq43CIUjiBCgOcHE1/HRoOIMeTiBDLYrhWt/"); 
 
-  crack("$6$WhyN0t$cPCekX8XV8LZtcl593QCRdxxo.GwC/LQTVayU5dz/yXSaEBJJLim9nKr4XBrCw9hiodp8fl3ilRXMM.HyfWW70"); // NA99
+  crack("$6$WhyN0t$cPCekX8XV8LZtcl593QCRdxxo.GwC/LQTVayU5dz/yXSaEBJJLim9nKr4XBrCw9hiodp8fl3ilRXMM.HyfWW70"); 
 
-  // crack("$6$WhyN0t$.12IafO67JQFVKeJDk51b5sNLXhWTM/wjrIUs.3IuMQM6E5sqmjFLDTzjbo6kbUm4qmzla4.As8JLco0KI5tm1"); // WC23
-
-  
 
   printf("%d solutions explored\n", count);
 
   omp_destroy_lock(&lock);
   clock_gettime(CLOCK_MONOTONIC, &finish);
   time_difference(&start, &finish, &difference);
-  printf("run lasted %9.5lfs\n", difference / 1000000000.0);
+  printf("program Time %9.5lfs\n", difference / 1000000000.0);
   return 0;
 }
--- makefile
+++ makefile
@@ -1,28 +1,13 @@
-all: CrackPassword CrackPassword2 EncryptPassword
+all: CrackPassword  EncryptPassword MatrixMultiply
 
 CrackPassword: CrackPassword.c time_diff.c time_diff.h
 	gcc -o crack CrackPassword.c time_diff.c -lrt -lcrypt -fopenmp
 
-CrackPassword2: CrackPass.c time_diff.c time_diff.h
-	cc -o crack02 CrackPass.c time_diff.c -lrt -lcrypt -fopenmp
+MatrixMultiply: MatrixMultiply.c 
+	gcc MatrixMultiply.c -o MatrixMultiply
 
 EncryptPassword: EncryptPassword.c 
 	cc -o encrypt EncryptPassword.c -lcrypt 
-# penny02: penny02.c time_diff.c time_diff.h
-# 	cc -o penny02 penny02.c time_diff.c -lrt -pthread
-# penny03: penny03.c time_diff.c time_diff.h
-# 	cc -o penny03 penny03.c time_diff.c -lrt -pthread
-# penny04: penny04.c time_diff.c time_diff.h
-# 	cc -o penny04 penny04.c time_diff.c -lrt -pthread
-# penny05: penny05.c time_diff.c time_diff.h
-# 	cc -o penny05 penny05.c time_diff.c -lrt -pthread
-# penny06: penny06.c time_diff.c time_diff.h
-# 	cc -o penny06 penny06.c time_diff.c -lrt -pthread
-# penny07: penny07.c time_diff.c time_diff.h
-# 	cc -o penny07 penny07.c time_diff.c -lrt -pthread
-# penny08: penny08.c time_diff.c time_diff.h
-# 	cc -o penny08 penny08.c time_diff.c -lrt -pthread
-# penny09: penny09.c time_diff.c time_diff.h
-# 	cc -o penny09 penny09.c time_diff.c -lrt -pthread
+
 clean:
 	rm -f CrackPassword *~ a.out
