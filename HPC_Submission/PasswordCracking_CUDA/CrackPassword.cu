3c7f522739a2cae834b0d58cf9e3b6b9c54256a6
NabinTako <nabintako999@gmail.com>
2024-01-12 22:00:26 +0545

    +CrackPasswordUsingCuda program

--- HPC_Submission/PasswordCracking_CUDA/CrackPassword.cu
+++ HPC_Submission/PasswordCracking_CUDA/CrackPassword.cu
@@ -1,170 +1,212 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <cuda_runtime_api.h>
-#include <crypt.h>
-#include "time_diff.h"
 
-// compile the program by, nvcc -Wno-deprecated-gpu-targets -o crackPassword CrackPassword.cu time_diff.c -lrt
-// or simply use make
+// Compile: nvcc -Wno-deprecated-gpu-targets -o crackPassword CrackPassword.cu -lrt  or just use make
+// Execute: ./crackPassword
 
+/** 
+ * The Program Cracks password combinations of <smallalphabet><smallalphabet><number><number>. Example: aa12,nt99,wc23 etc.
+ * The Program uses the power of GPU to run many threads parallelly n GPU. CPU first transferres data using the cudaRunTme library,
+ * also while transferring the data, it also checks for errors whle transferring the datas in the dynamic memories using 
+ * CheckForErrors function that takes type cudaError_t (a cuda error type) as a parameter and displays the error messages.
+ * 
+ * The man GPU function to decryptPassword is run by CPU thats why it has __global__ attribute and other functoin 
+ * with __device__ attribute in run by the GPU function. Among the gpu run functions, 
+ * CudaCrypt is used to encrypt the newly generated plain password then be passed as parameter to function ComparePassword where
+ * the newly encryptedPassword is compared with the userGivenEncryptedPassword and is true the function returns 1. 
+ * If the comparePassword Function returns one CopyDecryptedPassword function is called by the main function of GPU which
+ * copies the data of plainPassword and stores t on the gpu assigned pointer variable. 
+ * The data stored in Assigned pointer variable is then copied back to hte CPU pointer data usng cudaMemcpy function And dsplayed to the user.
+**/
+
+// Variables to track program run time
 struct timespec start, finish;
 long long int difference;
 
-__global__ void CrackPassword(char *encryptedPass,int* passwordFound)
-{
+// Functions To be run on Device/ GPU.
+__device__ char * CopyDecryptedPassword(char *dest,  char *src){
+  int i = 0;
+  do {
+    dest[i] = src[i];}
+  while (src[i++] != 0);
+  return dest;
+}
+// compares the given two character array adn returns either true or false.
+__device__ int ComparePassword( char *str_a,  char *str_b, unsigned len = 255){
+	int numberOfCharactermatched = 0;
+	int i = 0;
+	int outOfRange = 0;
+	while ((i < len) && !outOfRange) {
+		if ((str_a[i] == 0) || (str_b[i] == 0)) {
+			outOfRange = 1;
+		}
+		else if (str_a[i] == str_b[i]) {
+			numberOfCharactermatched++;
+		}
+		i++;
+	}
+// as numberOfCharactermatched is increased every loop when the password character matches 
+// and loop runs extra one time, so if match equals the number of loop minus 1, 
+// every password characters matched as variable numberOfCharactermatched is increased by 1 in every loop except the last one.
 
-    int x, y, z;                 // loop counters
-    char salt[11];               // String used in hashing the password.
-    char passwordCombination[7]; // The combination of letters currently being checked
-    char *enc;
-    int threadId = blockIdx.x * blockDim.x + threadIdx.x; // finding the threidId.
-    memcpy(salt, encryptedPass + 0, sizeof(salt));
-    salt[10] = '\0';
+	if(numberOfCharactermatched == i-1){ 
+		return 1;
+	}else{
+		return 0;
+	}
 
-    for (x = 'A' + threadId; x <= 'Z'; x += blockDim.x * gridDim.x)
-    {
-
-        for (y = 'A'; y <= 'Z'; y++)
-        {
-
-            for (z = 0; z <= 99; z++)
-            {
-
-                printf(passwordCombination, sizeof(passwordCombination), "%c%c%02d", x, y, z);
-                enc = (char *)crypt(passwordCombination, salt);
-
-                if (memcmp(encryptedPass, enc, 97) == 0)
-                {
-                    *passwordFound = 1; // set the flag variable to tell other threads that password is found and break the operation.
-                    break; // Break out of the innermost loop
-                }
-            }
-
-            if (*passwordFound == 1)
-            {
-                break;
-            }
-        }
-
-        if (*passwordFound == 1)
-        {
-            break;
-        }
-    }
-
-    memcpy(encryptedPass, passwordCombination + 0, sizeof(passwordCombination));
-    encryptedPass[10] = '\0';
 }
 
-int main()
-{
-    int blockNumber, threadNumber;
-    printf("Number of Blocks to use: ");
-    scanf("%d", &blockNumber);
-    printf("Number of Threads per Block: ");
-    scanf("%d", &threadNumber); // get the number of threads to use
+// used to encrypt the plaintext to encryptedText inside GPU.
+__device__ char* CudaCrypt(char* passwordToEncrypt){
 
-    clock_gettime(CLOCK_MONOTONIC, &start);
+	char * encryptedPassword = (char *) malloc(sizeof(char) * 11);
+
+    int z_AsciiValue = 122; // As ASCii value of Z is 90.
+    int a_AsciiValue = 97; // As ASCii value of A is 65.
+ 
+	encryptedPassword[0] = passwordToEncrypt[0] + 2;
+	encryptedPassword[1] = passwordToEncrypt[0] - 2;
+	encryptedPassword[2] = passwordToEncrypt[0] + 1;
+	encryptedPassword[3] = passwordToEncrypt[1] + 3;
+	encryptedPassword[4] = passwordToEncrypt[1] - 3;
+	encryptedPassword[5] = passwordToEncrypt[1] - 1;
+	encryptedPassword[6] = passwordToEncrypt[2] + 2;
+	encryptedPassword[7] = passwordToEncrypt[2] - 2;
+	encryptedPassword[8] = passwordToEncrypt[3] + 4;
+	encryptedPassword[9] = passwordToEncrypt[3] - 4;
+	encryptedPassword[10] = '\0';
+
+	for(int i =0; i<10; i++){
+		if(i >= 0 && i < 6){ //checking all lower case letter limits
+			if(encryptedPassword[i] > z_AsciiValue){
+				encryptedPassword[i] = (encryptedPassword[i] - z_AsciiValue) + a_AsciiValue;
+			}else if(encryptedPassword[i] < a_AsciiValue){
+				encryptedPassword[i] = (a_AsciiValue - encryptedPassword[i]) + a_AsciiValue;
+			}
+		}else{ //checking number section
+			if(encryptedPassword[i] > 57){
+				encryptedPassword[i] = (encryptedPassword[i] - 57) + 48;
+			}else if(encryptedPassword[i] < 48){
+				encryptedPassword[i] = (48 - encryptedPassword[i]) + 48;
+			}
+		}
+	}
+	return encryptedPassword;
+}
+
+// Called by the HOST/ CPU and run nsde the GPU/DEVICE.
+// Used to Crack  the encrypted password given by the user.
+__global__ void CrackPassword(char * alphabet, char * numbers, char * encryptedPassword) {
+
+	char rawPasswordToCheck[4];
+
+	rawPasswordToCheck[0] = alphabet[blockIdx.x];
+	rawPasswordToCheck[1] = alphabet[blockIdx.y];
+
+	rawPasswordToCheck[2] = numbers[threadIdx.x];
+	rawPasswordToCheck[3] = numbers[threadIdx.y];
+
+
+	if (ComparePassword(CudaCrypt(rawPasswordToCheck), encryptedPassword) == 1) {
+		CopyDecryptedPassword(encryptedPassword, rawPasswordToCheck);
+	}
+}
+
+// Functions To be run on Host/ CPU.
+// to calculate the program total run time
+int time_difference(struct timespec *start, struct timespec *finish, long long int *difference) {
+
+
+  long long int ds =  finish->tv_sec - start->tv_sec; 
+  long long int dn =  finish->tv_nsec - start->tv_nsec; 
+
+  if(dn < 0 ) {
+    ds--;
+    dn += 1000000000; 
+  } 
+  *difference = ds * 1000000000 + dn;
+  
+  return !(*difference > 0);
+}
+
+void CheckForError(cudaError_t error){
+     if (error)
+    {
+        printf("Something went wrong!\n");
+        printf("Error: %s\n",cudaGetErrorString(error));
+
+        clock_gettime(CLOCK_MONOTONIC, &finish);
+
+        time_difference(&start,&finish,&difference);
+        printf("Program run time: %lf\n", difference /1000000000.0 );
+
+        exit(1);
+    }
+}
+
+int main(){
+    clock_gettime(CLOCK_MONOTONIC,&start);
     cudaError_t error;
+    
+    
+	char alphabetArrayHost[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	char numberArrayHost[10] = {'0','1','2','3','4','5','6','7','8','9'};
 
-    // a shared flag to terminate the program when the password combination is found.
-    int passwordFoundHost = 0;
-    int *passwordFoundDevice;
+	char encryptedPassword[11] = "tpsmgi6291";
 
-    char *encryptedPasswordDevice;
+	char *decryptedPassword;
+	decryptedPassword = (char *)malloc(sizeof(char) * 26);
 
-    char encryptedPasswordHost[97] = "$6$WhyN0t$YHWmjxJ49Ob0xHhxZQuYsaJB5V8uukKIUYFnO.RSWKZOCB2H/i28hsPa2ibDXVicSHwAZUAkREqpc2P3066Fr1";
+	char * alphabetDataDevice;
+	error = cudaMalloc( (void**) &alphabetDataDevice, sizeof(char) * 26); 
+    CheckForError(error);
+    
+	error = cudaMemcpy(alphabetDataDevice, alphabetArrayHost, sizeof(char) * 26, cudaMemcpyHostToDevice);
+    CheckForError(error);
 
-    error = cudaMalloc(&encryptedPasswordDevice, sizeof(encryptedPasswordHost));
-    if (error)
-    {
-        fprintf(stderr, "cudaMalloc on encryptedPasswordDevice returned %d %s\n", error,
-                cudaGetErrorString(error));
-        exit(1);
-    }
+    char * numberDataDevice;
+	cudaMalloc( (void**) &numberDataDevice, sizeof(char) * 10); 
+    CheckForError(error);
 
-    error = cudaMemcpy(encryptedPasswordDevice, &encryptedPasswordHost, sizeof(encryptedPasswordHost), cudaMemcpyHostToDevice);
-    if (error)
-    {
-        fprintf(stderr, "cudaMemcpy to encryptedPasswordDevice returned %d %s\n", error,
-                cudaGetErrorString(error));
-    }
+	cudaMemcpy(numberDataDevice, numberArrayHost, sizeof(char) * 10, cudaMemcpyHostToDevice);
+    CheckForError(error);
+	
+    char *passwordToDecryptDevice;
+	cudaMalloc( (void**) &passwordToDecryptDevice, sizeof(char) * 26);
+    CheckForError(error);
 
-    // Copying the value of passwordfoundhost to be used in device.
-    error = cudaMalloc(&passwordFoundDevice, sizeof(int));
-    if (error)
-    {
-        fprintf(stderr, "cudaMalloc on encryptedPasswordDevice returned %d %s\n", error,
-                cudaGetErrorString(error));
-        exit(1);
-    }
+	cudaMemcpy(passwordToDecryptDevice, encryptedPassword, sizeof(char) * 26, cudaMemcpyHostToDevice);
+    CheckForError(error);
 
-    error = cudaMemcpy(passwordFoundDevice, &passwordFoundHost, sizeof(int), cudaMemcpyHostToDevice);
-    if (error)
-    {
-        fprintf(stderr, "cudaMemcpy to encryptedPasswordDevice returned %d %s\n", error,
-                cudaGetErrorString(error));
-    }
+    dim3 numberOfBlocksToUse = dim3(26,26,1); 
+    dim3 threadsPerBlocks = dim3(10,10,1); 
 
-    CrackPassword<<<blockNumber, threadNumber>>>(encryptedPasswordDevice,passwordFoundDevice);
-    cudaDeviceSynchronize();
+    CrackPassword<<<numberOfBlocksToUse, threadsPerBlocks >>>( alphabetDataDevice, numberDataDevice, passwordToDecryptDevice );
+	cudaDeviceSynchronize();  
+    
+	cudaMemcpy(decryptedPassword, passwordToDecryptDevice, sizeof(char) * 26, cudaMemcpyDeviceToHost);
+    CheckForError(error);
 
-    error = cudaMemcpy(&passwordFoundHost, passwordFoundDevice, sizeof(int), cudaMemcpyDeviceToHost);
-    if (error)
-    {
-        fprintf(stderr, "cudaMemcpy to passwordFoundHost returned %d %s\n", error,
-        cudaGetErrorString(error));
-    }
-    if(passwordFoundHost == 1){
-        error = cudaMemcpy(encryptedPasswordHost, encryptedPasswordDevice, sizeof(encryptedPasswordHost), cudaMemcpyDeviceToHost);
-        if (error)
-        {
-            fprintf(stderr, "cudaMemcpy to encryptedPasswordHost returned %d %s\n", error,
-            cudaGetErrorString(error));
-        }
-    }else{
-        printf("Password not found!!!\n");
-    }
+	printf("\nUser Given Encrypted Password: %s,\tDecrypted Password: %s\n\n", encryptedPassword, decryptedPassword);
 
-    // free the device pointers
-    error = cudaFree(encryptedPasswordDevice);
-    if (error)
-    {
-        fprintf(stderr, "cudaFree on encryptedPasswordDevice returned %d %s\n", error,
-        cudaGetErrorString(error));
-        exit(1);
-    }
-    error = cudaFree(passwordFoundDevice);
-    if (error)
-    {
-        fprintf(stderr, "cudaFree on passwordFoundDevice returned %d %s\n", error,
-        cudaGetErrorString(error));
-        exit(1);
-    }
+    // free the pointer memory
+	free(decryptedPassword);
+	error = cudaFree(alphabetDataDevice);
+    CheckForError(error);
 
-    printf("result: encryptedPasswordHost = %s\n", encryptedPasswordHost);
+	error = cudaFree(numberDataDevice);
+    CheckForError(error);
+
+	error = cudaFree(passwordToDecryptDevice);
+    CheckForError(error);
 
     clock_gettime(CLOCK_MONOTONIC, &finish);
 
-    // canculating the time taken by the program to find the unique password.
-    time_difference(&start, &finish, &difference);
+    time_difference(&start,&finish,&difference);
+    printf("Program run time: %lf\n\n", difference /1000000000.0 );
 
-    printf("program Run Time %9.5lfs\n", difference / 1000000000.0);
-    return 0;
+	return 0;
 }
 
-// to calculate the total run time of the program
-int time_difference(struct timespec *start, struct timespec *finish, long long int *difference)
-{
-    long long int ds = finish->tv_sec - start->tv_sec;
-    long long int dn = finish->tv_nsec - start->tv_nsec;
-
-    if (dn < 0)
-    {
-        ds--;
-        dn += 1000000000;
-    }
-    *difference = ds * 1000000000 + dn;
-
-    return !(*difference > 0);
-}
---
+++ HPC_Submission/PasswordCracking_CUDA/Encrypt.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+// Compile: gcc Encrypt.c -o EncryptPassword  or just use make
+// Execute: ./EncryptPassword
+
+char* Crypt(char* rawPassword){
+
+	char * newPassword = (char *) malloc(sizeof(rawPassword));
+
+    int z_AsciiValue = 122; // As ASCii value of Z is 90.
+    int a_AsciiValue = 97; // As ASCii value of A is 65.
+ 
+	newPassword[0] = rawPassword[0] + 2;
+	newPassword[1] = rawPassword[0] - 2;
+	newPassword[2] = rawPassword[0] + 1;
+	newPassword[3] = rawPassword[1] + 3;
+	newPassword[4] = rawPassword[1] - 3;
+	newPassword[5] = rawPassword[1] - 1;
+	newPassword[6] = rawPassword[2] + 2;
+	newPassword[7] = rawPassword[2] - 2;
+	newPassword[8] = rawPassword[3] + 4;
+	newPassword[9] = rawPassword[3] - 4;
+	newPassword[10] = '\0';
+
+	for(int i =0; i<10; i++){
+		if(i >= 0 && i < 6){ 
+			if(newPassword[i] > z_AsciiValue){
+				newPassword[i] = (newPassword[i] - z_AsciiValue) + a_AsciiValue;
+			}else if(newPassword[i] < a_AsciiValue){
+				newPassword[i] = (a_AsciiValue- newPassword[i]) + a_AsciiValue;
+			}
+		}else{ //checking number section
+			if(newPassword[i] > 57){
+				newPassword[i] = (newPassword[i] - 57) + 48;
+			}else if(newPassword[i] < 48){
+				newPassword[i] = (48 - newPassword[i]) + 48;
+			}
+		}
+	}
+	return newPassword;
+}
+
+int main(int argc, char  *argv[])
+{
+    
+    char * test= Crypt(argv[1]);
+
+    printf("%s\n",test);
+    free(test);
+}
