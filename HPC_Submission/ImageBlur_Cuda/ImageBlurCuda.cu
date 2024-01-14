e1c1e9084898677c7480059a0046b8d7dbd2d6c6
NabinTako <nabintako999@gmail.com>
2024-01-14 20:14:41 +0545

    + image blur, +Submission_2_Zip File

--- HPC_Submission/ImageBlur_Cuda/ImageBlurCuda.cu
+++ HPC_Submission/ImageBlur_Cuda/ImageBlurCuda.cu
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cuda_runtime_api.h>
 #include "lodepng.h"
 
-/*****************
+//compile : nvcc -Wno-deprecated-gpu-targets -o ImageBlur ImageBlurCuda.cu lodepng.cpp  or just use make
+// Execute :./ImageBlur
 
-    command to compile
-	nvcc -Wno-deprecated-gpu-targets -o ImageBlur ImageBlur.cu lodepng.cpp 
-    // run - ./ImageBlur
-
- *****************/
+	
+/**
+ * Program takes a PNG image type as input then gives out a blured image of hte original image in the same location 
+ * as blurImage.png image.
+ * 
+ * ImageBlur function is called by HOST and run on DEVICE. This function is responsible for making hte blur image of the user given image.
+ * the function takes 5 parameter that includes the data to be processed and the pointer to save the new blur data along with the 
+ * width and height of the image and by how much to blurl the original image. 
+**/
 
 
 
@@ -19,8 +25,8 @@ __global__ void ImageBlur(unsigned char * imageOutPutDevice, unsigned char * ima
 	int red = 0;
 	int green = 0;
 	int blue = 0;
+	int a = 0;
 	int x,y;
-	int C = 0;
 
 
 	int idx = blockDim.x * blockIdx.x + threadIdx.x;
@@ -32,15 +38,15 @@ __global__ void ImageBlur(unsigned char * imageOutPutDevice, unsigned char * ima
 					red += imageDataHost[y];
 					green += imageDataHost[1+y];
 					blue += imageDataHost[2+y]; 
-					C++;
+					a++;
 				}
 			}
 		}
 	}
 
-	imageOutPutDevice[pixel] = red / C;
-	imageOutPutDevice[1+pixel] = green / C;
-	imageOutPutDevice[2+pixel] = blue / C;
+	imageOutPutDevice[pixel] = red / a;
+	imageOutPutDevice[1+pixel] = green / a;
+	imageOutPutDevice[2+pixel] = blue / a;
 	imageOutPutDevice[3+pixel] = imageDataHost[3+pixel];
 }
 // to check for errors during cudaFunctions
@@ -55,14 +61,14 @@ void CheckForError(cudaError_t error){
 }
 
 int main(int argc, char **argv){
-    cudaError_t cudaError;
+    cudaError_t cudaError; // to check for error during cuda functions.
     int blurAmount = 1; // To Determine the blur effects of picture
-	unsigned int error;
+	unsigned int error; // to check for error during encoding and decoding the image data.
 	unsigned char* img;
 	unsigned int imageWidth;
 	unsigned int imageHeight;
-	const char* userInputImageName = "test.png";  //the image given by user
-	const char* programOutputImage = "testBlur.png";//the blur image created by program
+	const char* userInputImageName = "originalImage.png";  // image given by the user.
+	const char* programOutputImage = "blurImageOutput.png";  // blur image created by the program.
 
 	error = lodepng_decode32_file(&img, &imageWidth, &imageHeight, userInputImageName);
 	if(error){
@@ -86,28 +92,27 @@ int main(int argc, char **argv){
 // Device memory
 	cudaError = cudaMalloc((void**) &imageDataDevice, ARRAY_BYTES);
     CheckForError(cudaError);
+
 	cudaError = cudaMalloc((void**) &imageDataHost, ARRAY_BYTES);
     CheckForError(cudaError);
-
-
 
 	cudaError = cudaMemcpy(imageDataDevice, host_imgInput, ARRAY_BYTES, cudaMemcpyHostToDevice);
     CheckForError(cudaError);
 
-
+	// Running the GPU funtion
 	ImageBlur<<<imageHeight, imageWidth>>>(imageDataHost, imageDataDevice, imageWidth, imageHeight,blurAmount);
 
 	cudaError = cudaMemcpy(host_imgOutput, imageDataHost, ARRAY_BYTES, cudaMemcpyDeviceToHost);
     CheckForError(cudaError);
 
-	printf("The blur Image has been created!\n");
+	printf("The blur Image has been created!!\n");
 	
 	error = lodepng_encode32_file(programOutputImage, host_imgOutput, imageWidth, imageHeight);
 	if(error){
 		printf("error occured %u: %s\n", error, lodepng_error_text(error));
 	}
 
-// free dynamic memory
+	// free dynamic memory
 	cudaError = cudaFree(imageDataDevice);
     CheckForError(cudaError);
 	cudaError = cudaFree(imageDataHost);
